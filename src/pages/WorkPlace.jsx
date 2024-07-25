fc162d7a39247f206a1962a54d90673d06108ee0
GaboSnipe <gabriel.gulua@geolab.edu.ge>
2024-07-25 16:49:07 +0400

    redux start

--- src/pages/WorkPlace.jsx
+++ src/pages/WorkPlace.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 
 const WorkPlace = () => {
   return (
     <>
     <div className="min-h-full">
         <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1>Workplace</h1>
+
         </div>
     </div>
   </>
