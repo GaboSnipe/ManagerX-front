0a44d2dffdfbb7cdba77a65373a01b1b6f20b3e3
dimitri_gulua <dimitri_gulua@singular.uk>
2024-09-07 12:25:02 +0400

    update project page for new services

--- src/services/ProjectsService.js
+++ src/services/ProjectsService.js
@@ -2,13 +2,13 @@ import $api from "../http";
 
 export default class ProjectsService {
     static async getProjectList (){
-        return $api.get('/api/expertise/folder/list/')
+        return $api.get('/api/expertise/data/list/')
     }    
     static async getProjectDetails (uuid){
         return $api.get(`/api/expertise/folder/${uuid}/details/`)
     }    
     static async getProjectHeaders (){
-        return $api.get(`/api/expertise/folder/custom-fields/list/`)
+        return $api.get(`/api/expertise/data/custom-fields/list/`)
     }
 }
 
