67b5f01dcc4b7e52a3c30b2a804f2f2d0e7249c8
dimitri_gulua <dimitri_gulua@singular.uk>
2024-10-15 22:58:06 +0400

    change react-comments-section source

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,26 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteCommonjs(),
+  ],
   server: {
     host: '0.0.0.0',
     port: 3000,
+    open: true,
   },
   define: {
-    // Some libraries use the global object, even though it doesn't exist in the browser.
-    // Alternatively, we could add `<script>window.global = window;</script>` to index.html.
-    // https://github.com/vitejs/vite/discussions/5912
     global: {},
+  },
+  build: {
+    outDir: 'dist',
+    minify: 'terser',
+    sourcemap: true,
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom'],
   },
 });
