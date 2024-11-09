e004504a17f3c9f280c0e936967d027288e6e5a8
gabosnipe <gabriel.gulua@geolab.edu.ge>
2024-11-09 19:40:43 +0400

    redis notes

--- src/components/Notes/NotesListComponent.jsx
+++ src/components/Notes/NotesListComponent.jsx
@@ -5,11 +5,11 @@ const NotesListComponent = ({ notesList }) => {
   const [selectedNote, setSelectedNote] = useState(null);
 
   return (
-    <div className="flex h-full p-6 bg-gray-100 rounded-lg shadow-lg space-y-4 m-8">
-      <div className="flex flex-col bg-grat-100 p-4 w-64 mr-4 rounded-lg shadow-lg space-y-2 overflow-y-auto">
+    <div className="flex p-6 bg-gray-100 min-h-[52rem] rounded-lg shadow-lg space-y-4 m-8">
+      <div className="flex flex-col bg-grat-100 w-64 mr-4 rounded-lg space-y-2 overflow-y-auto">
         <AddNewNoteButton />
         {notesList.map((note) => (
-          <NoteNode key={note.id} note={note} setSelectedNote={setSelectedNote} />
+          <NoteNode key={note.id} note={note} setSelectedNote={setSelectedNote} isSelected={selectedNote?.id === note?.id} />
         ))}
       </div>
       <div className="w-full h-full p-4 rounded-lg ">
