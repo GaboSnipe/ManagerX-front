a08cf2376a5e9870e45407c8006428cb720dce62
GaboSnipe <gabriel.gulua@geolab.edu.ge>
2024-11-15 14:52:38 +0400

    ragac

--- src/components/Notes/NotesListComponent.jsx
+++ src/components/Notes/NotesListComponent.jsx
@@ -5,15 +5,15 @@ const NotesListComponent = ({ notesList }) => {
   const [selectedNote, setSelectedNote] = useState(null);
 
   return (
-    <div className="flex h-full p-6 bg-gray-100 rounded-lg shadow-lg space-y-4 m-8">
-      <div className="flex flex-col bg-grat-100 p-4 w-64 mr-4 rounded-lg shadow-lg space-y-2 overflow-y-auto">
+    <div className="flex p-6 bg-gray-100 min-h-[52rem] rounded-lg shadow-lg space-y-4 m-8">
+      <div className="flex flex-col bg-grat-100 w-64 mr-4 rounded-lg space-y-2 overflow-y-auto">
         <AddNewNoteButton />
         {notesList.map((note) => (
           <NoteNode key={note.id} note={note} setSelectedNote={setSelectedNote} />
         ))}
       </div>
       <div className="w-full h-full p-4 rounded-lg ">
-        <NoteEditor selectedNote={selectedNote} />
+      <NoteNode key={note.id} note={note} setSelectedNote={setSelectedNote} isSelected={selectedNote?.id === note?.id} />
         <div className="flex justify-center">
           <button className='bg-gray-200 m-2 w-24 rounded-sm text-gray-500 hover:bg-gray-300 hover:text-gray-600 transition duration-300'>
             save
--- src/components/Notes/components/NoteNode.jsx
+++ src/components/Notes/components/NoteNode.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const NoteNode = ({ note, setSelectedNote }) => {
+const NoteNode = ({ note, setSelectedNote, isSelected }) => {
   return (
     <button 
       onClick={() => setSelectedNote(note)}
-      className="bg-blue-100 hover:bg-blue-200 text-blue-800 font-medium py-2 px-4 rounded-lg flex items-center justify-center transition duration-200 ease-in-out"
-    >
+      className={`hover:bg-blue-200 text-blue-800 font-medium py-2 px-4 rounded-lg flex items-center transition duration-200 ease-in-out ${isSelected && "bg-blue-200"}` }
+      >
       <span className="text-sm truncate">{note.title}</span>
     </button>
   );
